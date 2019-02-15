f1410ab1fe2ad2568038d7890ce6a859018b00f8
nmferreira <nmferreira@gmail.com>
2019-02-15 12:45:54 -0200

    aula 02

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Container(
-      color: Colors.blueGrey
-    )
-  ));
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                color: Colors.amberAccent,
+                fontStyle: FontStyle.italic,
+                fontSize: 30),
+          )
+        ],
+      )));
 }
