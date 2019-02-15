a19101260a2a1bddbfdc73c05d1495aa93683e1d
nmferreira <nmferreira@gmail.com>
2019-02-15 13:02:57 -0200

    aula 03

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ void main() {
             "Pessoas: 0",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text("+1",
+                    style: TextStyle(fontSize: 40, color: Colors.green[400])),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text("-1",
+                    style: TextStyle(fontSize: 40, color: Colors.red[400])),
+                onPressed: () {},
+              ),
+            ],
+          ),
+
           Text(
             "Pode entrar!",
             style: TextStyle(
